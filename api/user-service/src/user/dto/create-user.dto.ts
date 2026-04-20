b180e49f1e7a9427ddc23950d6f39475b36dd077
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-04-20 11:30:42 +0700

    feat: edit createUserDTO

--- api/user-service/src/user/dto/create-user.dto.ts
+++ api/user-service/src/user/dto/create-user.dto.ts
@@ -1,1 +1,30 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+import { Role } from '../../generated/prisma/client';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  phone: string;
+
+  @IsEnum(Role)
+  @IsOptional()
+  role?: Role;
+}
