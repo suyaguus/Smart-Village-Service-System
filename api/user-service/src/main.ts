66cee4ad2777e61221780b0d7c56f2b0983fd11e
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-04-20 13:50:40 +0700

    fix route code

--- api/user-service/src/main.ts
+++ api/user-service/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // menambahkan prefix globa
-  app.setGlobalPrefix('api/users');
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT!);
 }
 void bootstrap();
