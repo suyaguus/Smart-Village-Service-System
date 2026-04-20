b09e4f40021bcfd1cb3a5dd7b341fee18a8855ad
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-04-20 14:20:05 +0700

    feat: menambahkan async untuk findOne

--- api/user-service/src/user/user.service.ts
+++ api/user-service/src/user/user.service.ts
@@ -94,7 +94,7 @@ export class UserService {
     };
   }
 
-  findOne(id: string) {
+  async findOne(id: string) {
     // return `This action returns a #${id} user`;
 
     // membuat fungsi untuk mengambil data user berdasarkan id dari database
