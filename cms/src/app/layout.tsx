de0eb9beda2947167be9c95123212653bfa79011
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-04-21 12:07:19 +0700

    feat: import queryProvider

--- cms/src/app/layout.tsx
+++ cms/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
-import QueryProvider from "@/components/providers/QueryProvider";
+import QueryProvider from "../components/providers/QueryProvider";
 import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
