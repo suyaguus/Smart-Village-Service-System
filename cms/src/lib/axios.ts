7576d366b8aac477eac91c0781b8f24b26ab7636
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-04-21 12:10:18 +0700

    feat: menambahkan interceptor untuk menangani respons dan error di axios.ts

--- cms/src/lib/axios.ts
+++ cms/src/lib/axios.ts
@@ -21,3 +21,18 @@ apiClient.interceptors.request.use((config) => {
   //   kembalikan konfigurasi yang telah dimodifikasi
   return config;
 });
+
+// menambahkan interceptor untuk menangani respons dan error
+apiClient.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            Cookies.remove("token");
+            window.location.href = "/login";
+        }
+
+        return Promise.reject(error);
+    }
+);
+
+export default apiClient;
