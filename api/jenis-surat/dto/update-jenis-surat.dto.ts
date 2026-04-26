cb21a80ce525b42543055ac9dbf2995aa4ce8b6c
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-04-26 23:27:44 +0700

    delete folder jenis-surat

--- api/jenis-surat/dto/update-jenis-surat.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateJenisSuratDto } from './create-jenis-surat.dto';
-
-export class UpdateJenisSuratDto extends PartialType(CreateJenisSuratDto) {}
--- api/jenis-surat/jenis-surat.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { JenisSuratService } from './jenis-surat.service';
-import { JenisSuratController } from './jenis-surat.controller';
-
-@Module({
-  controllers: [JenisSuratController],
-  providers: [JenisSuratService],
-})
-export class JenisSuratModule {}
