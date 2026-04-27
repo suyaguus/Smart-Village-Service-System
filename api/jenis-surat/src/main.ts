d62cea25f09e494bfe427beddd5fe190c7ac5eef
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-04-27 13:25:47 +0700

    feat: menambahkan validation pipi, import dotenv

--- api/jenis-surat/src/main.ts
+++ api/jenis-surat/src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // menambahkan prefix globa
+  // validationPipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  // menambahkan prefix global
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT!);
 }
