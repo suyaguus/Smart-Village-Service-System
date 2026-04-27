41a4878cf9b1a11b015d37059b6183a703f8fa22
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-04-27 13:31:16 +0700

    feat: validasi create jenis-surat dto

--- api/jenis-surat/src/jenis-surat/dto/create-jenis-surat.dto.ts
+++ api/jenis-surat/src/jenis-surat/dto/create-jenis-surat.dto.ts
@@ -1,1 +1,15 @@
-export class CreateJenisSuratDto {}
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateJenisSuratDto {
+  @IsString()
+  @IsNotEmpty()
+  nama_surat!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  kode_surat!: string;
+
+  @IsString()
+  @IsOptional()
+  deskripsi?: string;
+}
