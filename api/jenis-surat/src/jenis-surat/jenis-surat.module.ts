aa02fcca8af051149122ad9787feb050d409356e
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-04-28 11:43:01 +0700

    feat: import prisma service ke jenis-surat module

--- api/jenis-surat/src/jenis-surat/jenis-surat.module.ts
+++ api/jenis-surat/src/jenis-surat/jenis-surat.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { JenisSuratService } from './jenis-surat.service';
 import { JenisSuratController } from './jenis-surat.controller';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [JenisSuratController],
-  providers: [JenisSuratService],
+  providers: [JenisSuratService, PrismaService],
 })
 export class JenisSuratModule {}
