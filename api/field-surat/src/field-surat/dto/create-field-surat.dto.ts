c4b0a0b5e2940a80e5e1a9f5f595b123252245f0
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-04-29 11:45:57 +0700

    feat: membuat create dto field-surat

--- api/field-surat/src/field-surat/dto/create-field-surat.dto.ts
+++ api/field-surat/src/field-surat/dto/create-field-surat.dto.ts
@@ -1,1 +1,43 @@
-export class CreateFieldSuratDto {}
+import {
+  IsBoolean,
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Min,
+} from 'class-validator';
+import { FieldType } from '../../generated/prisma/client';
+
+export class CreateFieldSuratDto {
+  @IsInt()
+  @Min(1)
+  jenis_surat_id!: number;
+
+  @IsString()
+  @IsNotEmpty()
+  field_name!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  field_label!: string;
+
+  @IsEnum(FieldType)
+  field_type!: FieldType;
+
+  @IsBoolean()
+  @IsOptional()
+  is_required?: boolean;
+
+  @IsInt()
+  @Min(1)
+  field_order!: number;
+
+  @IsInt()
+  @IsOptional()
+  max_file_size?: number;
+
+  @IsString()
+  @IsOptional()
+  accepted_format?: string;
+}
