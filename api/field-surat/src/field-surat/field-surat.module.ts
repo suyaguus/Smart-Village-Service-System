ba6339bace087c50b717a6ba2452bd8e300072e1
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-04-29 11:38:24 +0700

    feat: import prisma-service

--- api/field-surat/src/field-surat/field-surat.module.ts
+++ api/field-surat/src/field-surat/field-surat.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FieldSuratService } from './field-surat.service';
 import { FieldSuratController } from './field-surat.controller';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [FieldSuratController],
-  providers: [FieldSuratService],
+  providers: [FieldSuratService, PrismaService],
 })
 export class FieldSuratModule {}
