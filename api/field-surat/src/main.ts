904f30979b22790e4b571b2409c85193e81ec073
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-04-29 11:41:32 +0700

    feat: membuat validasiPipe

--- api/field-surat/src/main.ts
+++ api/field-surat/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // validationPipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
