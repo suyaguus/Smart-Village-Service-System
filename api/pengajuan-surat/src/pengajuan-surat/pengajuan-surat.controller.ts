6660dcb6a14d431bfc346411852ba7b1dd313b63
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-05-03 13:03:11 +0700

    feat: membuat pengajuan-surat controller, get by user id

--- api/pengajuan-surat/src/pengajuan-surat/pengajuan-surat.controller.ts
+++ api/pengajuan-surat/src/pengajuan-surat/pengajuan-surat.controller.ts
@@ -1,20 +1,55 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseUUIDPipe,
+  BadRequestException,
+  HttpStatus,
+} from '@nestjs/common';
 import { PengajuanSuratService } from './pengajuan-surat.service';
 import { CreatePengajuanSuratDto } from './dto/create-pengajuan-surat.dto';
-import { UpdatePengajuanSuratDto } from './dto/update-pengajuan-surat.dto';
+import { UpdateStatusDto } from './dto/update-pengajuan-surat.dto';
 
 @Controller('pengajuan-surat')
 export class PengajuanSuratController {
   constructor(private readonly pengajuanSuratService: PengajuanSuratService) {}
 
+  // endpoint untuk membuat pengajuan surat baru
   @Post()
   create(@Body() createPengajuanSuratDto: CreatePengajuanSuratDto) {
     return this.pengajuanSuratService.create(createPengajuanSuratDto);
   }
 
+  // endpoint untuk mendapatkan semua pengajuan surat
   @Get()
   findAll() {
     return this.pengajuanSuratService.findAll();
+  }
+
+  // tambahkan endpoint untuk mendapatkan pengajuan surat berdasarkan user_id
+  @Get('user/:user_id')
+  findByUser(
+    @Param(
+      'user_id',
+      new ParseUUIDPipe({
+        exceptionFactory: () => {
+          new BadRequestException({
+            success: false,
+            message: process.env.BAD_REQUEST_MESSAGE,
+            metadata: {
+              status: HttpStatus.BAD_REQUEST,
+            },
+          });
+        },
+      }),
+    )
+    user_id: string,
+  ) {
+    return this.pengajuanSuratService.findByUser(user_id);
   }
 
   @Get(':id')
@@ -23,7 +58,10 @@ export class PengajuanSuratController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePengajuanSuratDto: UpdatePengajuanSuratDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updatePengajuanSuratDto: UpdatePengajuanSuratDto,
+  ) {
     return this.pengajuanSuratService.update(+id, updatePengajuanSuratDto);
   }
 
