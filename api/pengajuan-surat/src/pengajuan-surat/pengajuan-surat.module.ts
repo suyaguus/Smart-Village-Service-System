950855e5dfdb02fef9738a862b3f0b7fff25b0d2
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-05-03 13:14:20 +0700

    feat: menambahkan prisma-service ke provider module

--- api/pengajuan-surat/src/pengajuan-surat/pengajuan-surat.module.ts
+++ api/pengajuan-surat/src/pengajuan-surat/pengajuan-surat.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PengajuanSuratService } from './pengajuan-surat.service';
 import { PengajuanSuratController } from './pengajuan-surat.controller';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [PengajuanSuratController],
-  providers: [PengajuanSuratService],
+  providers: [PengajuanSuratService, PrismaService],
 })
 export class PengajuanSuratModule {}
