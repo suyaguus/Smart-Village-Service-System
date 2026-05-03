3e13446113558495b2343899d888c17cf6df3de7
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-05-03 10:20:15 +0700

    feat: membuat dto create untuk pengajuan-surat

--- api/pengajuan-surat/src/pengajuan-surat/dto/create-pengajuan-surat.dto.ts
+++ api/pengajuan-surat/src/pengajuan-surat/dto/create-pengajuan-surat.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePengajuanSuratDto {}
+import { IsNotEmpty, IsUUID } from 'class-validator';
+
+// DTO untuk membuat pengajuan surat
+export class CreatePengajuanSuratDto {
+  @IsUUID()
+  @IsNotEmpty()
+  user_id!: string;
+
+  @IsUUID()
+  @IsNotEmpty()
+  jenis_surat_id!: string;
+}
