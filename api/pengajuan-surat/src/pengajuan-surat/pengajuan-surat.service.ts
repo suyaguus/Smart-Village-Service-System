edcadea5a6444ffc01b9b723fa6426562307e360
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-05-04 09:42:49 +0700

    refactor: mengganti pesan response

--- api/pengajuan-surat/src/pengajuan-surat/pengajuan-surat.service.ts
+++ api/pengajuan-surat/src/pengajuan-surat/pengajuan-surat.service.ts
@@ -273,7 +273,9 @@ export class PengajuanSuratService {
       // jika error lainnya, maka throw BadRequestException
       throw new BadRequestException({
         success: false,
-        message: 'Request tidak valid!',
+        // message: 'Request tidak valid!',
+        // refactor message response
+        message: process.env.BAD_REQUEST_MESSAGE,
         metadata: { status: HttpStatus.BAD_REQUEST },
       });
     }
