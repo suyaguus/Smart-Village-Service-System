a038145fa9d59c0bda56c32919f8d557639eef39
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-05-04 11:06:02 +0700

    feat: membuat prisma service

---
+++ api/pengaduan/src/prisma.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from './generated/prisma/client';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL as string,
+    });
+    super({ adapter });
+  }
+}
