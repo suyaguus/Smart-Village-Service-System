635277f0b29e3d4b8d8e9fb05a1e213cea180276
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-05-04 13:15:21 +0700

    feat: import prisma service

--- api/pengaduan/src/pengaduan/pengaduan.module.ts
+++ api/pengaduan/src/pengaduan/pengaduan.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PengaduanService } from './pengaduan.service';
 import { PengaduanController } from './pengaduan.controller';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [PengaduanController],
-  providers: [PengaduanService],
+  providers: [PengaduanService, PrismaService],
 })
 export class PengaduanModule {}
