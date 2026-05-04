a3369cc74009ec3e875b15885d4d8cbddc1e77dc
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-05-04 11:10:17 +0700

    feat: membuat cto create

--- api/pengaduan/src/pengaduan/dto/create-pengaduan.dto.ts
+++ api/pengaduan/src/pengaduan/dto/create-pengaduan.dto.ts
@@ -1,1 +1,15 @@
-export class CreatePengaduanDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreatePengaduanDto {
+  @IsString()
+  @IsNotEmpty()
+  user_id!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  judul!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  deskripsi!: string;
+}
