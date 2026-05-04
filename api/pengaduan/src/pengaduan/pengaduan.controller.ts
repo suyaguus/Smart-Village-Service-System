b9b1a3cc53e6211c9f358ac2bb2d6b8b5235b480
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-05-04 11:23:57 +0700

    fix: membuat custom parse int pipe

--- api/pengaduan/src/pengaduan/pengaduan.controller.ts
+++ api/pengaduan/src/pengaduan/pengaduan.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  BadRequestException,
+  ParseIntPipe,
+  HttpStatus,
+} from '@nestjs/common';
 import { PengaduanService } from './pengaduan.service';
 import { CreatePengaduanDto } from './dto/create-pengaduan.dto';
 import { UpdatePengaduanDto } from './dto/update-pengaduan.dto';
@@ -7,15 +18,33 @@ import { UpdatePengaduanDto } from './dto/update-pengaduan.dto';
 export class PengaduanController {
   constructor(private readonly pengaduanService: PengaduanService) {}
 
+  // endpoint untuk membuat pengaduan baru
   @Post()
   create(@Body() createPengaduanDto: CreatePengaduanDto) {
     return this.pengaduanService.create(createPengaduanDto);
   }
 
+  // endpoint untuk mendapatkan semua pengaduan
   @Get()
   findAll() {
     return this.pengaduanService.findAll();
   }
+
+  // membuat custom ParseIntPipe untuk override pesan error default
+  IntParam = (property: string) =>
+    Param(
+      property,
+      new ParseIntPipe({
+        exceptionFactory: () =>
+          new BadRequestException({
+            success: false,
+            message: process.env.BAD_REQUEST_MESSAGE,
+            metadata: {
+              status: HttpStatus.BAD_REQUEST,
+            },
+          }),
+      }),
+    );
 
   @Get(':id')
   findOne(@Param('id') id: string) {
@@ -23,7 +52,10 @@ export class PengaduanController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePengaduanDto: UpdatePengaduanDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updatePengaduanDto: UpdatePengaduanDto,
+  ) {
     return this.pengaduanService.update(+id, updatePengaduanDto);
   }
 
