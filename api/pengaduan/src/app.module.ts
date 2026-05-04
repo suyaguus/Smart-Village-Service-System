6298beaca71d91eda66cd91e142680bd218d3eb9
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-05-04 10:59:03 +0700

    import pengaduan resource

--- api/pengaduan/src/app.module.ts
+++ api/pengaduan/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PengaduanModule } from './pengaduan/pengaduan.module';
 
 @Module({
-  imports: [],
+  imports: [PengaduanModule],
   controllers: [AppController],
   providers: [AppService],
 })
