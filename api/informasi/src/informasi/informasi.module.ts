9a0bffef27fa621d102fae8fb686a05a92e5eff9
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-05-06 14:42:16 +0700

    feat: import prisma service ke module

--- api/informasi/src/informasi/informasi.module.ts
+++ api/informasi/src/informasi/informasi.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { InformasiService } from './informasi.service';
 import { InformasiController } from './informasi.controller';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [InformasiController],
-  providers: [InformasiService],
+  providers: [InformasiService, PrismaService],
 })
 export class InformasiModule {}
