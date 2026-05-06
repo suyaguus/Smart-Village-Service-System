ffb183428d1837c351070291138c62426f5343ec
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-05-06 11:01:53 +0700

    feat: membuat refactor untuk select data

---
+++ api/pengaduan/src/common/constants/select.ts
@@ -0,0 +1,9 @@
+// membuat refacotor untuk select data pengaduan
+export const PENGADUAN_LIST_SELECT = {
+  id: true,
+  user_id: true,
+  judul: true,
+  status: true,
+  created_at: true,
+  updated_at: true,
+};
