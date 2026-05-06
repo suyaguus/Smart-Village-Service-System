fd62f7e3ef6b3d12e22f0d280de76747ccc3a2eb
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-05-06 13:43:41 +0700

    feat: membuat constructor

--- api/informasi/src/informasi/informasi.service.ts
+++ api/informasi/src/informasi/informasi.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { CreateInformasiDto } from './dto/create-informasi.dto';
 import { UpdateInformasiDto } from './dto/update-informasi.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class InformasiService {
+  // buat constructor untuk inject PrismaService
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createInformasiDto: CreateInformasiDto) {
     return 'This action adds a new informasi';
   }
