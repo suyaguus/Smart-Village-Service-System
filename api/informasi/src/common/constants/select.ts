df635b34b894078451cd000f66df5153cc6e1034
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-05-06 15:20:43 +0700

    refactor: membuat informasi select

---
+++ api/informasi/src/common/constants/select.ts
@@ -0,0 +1,10 @@
+// refactor: update select object for informasi
+export const INFORMASI_SELECT = {
+  id: true,
+  admin_id: true,
+  judul: true,
+  isi: true,
+  foto: true,
+  created_at: true,
+  updated_at: true,
+};
