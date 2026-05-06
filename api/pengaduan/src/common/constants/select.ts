00c81ab028c55500568adcb402c3d3cbe94da2bd
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-05-06 11:02:24 +0700

    feat: membuat refactor untuk pengaduan select

--- api/pengaduan/src/common/constants/select.ts
+++ api/pengaduan/src/common/constants/select.ts
@@ -7,3 +7,22 @@ export const PENGADUAN_LIST_SELECT = {
   created_at: true,
   updated_at: true,
 };
+
+export const PENGADUAN_SELECT = {
+  id: true,
+  user_id: true,
+  judul: true,
+  deskripsi: true,
+  status: true,
+  created_at: true,
+  updated_at: true,
+  respon: {
+    select: {
+      id: true,
+      admin_id: true,
+      text: true,
+      created_at: true,
+    },
+    orderBy: { created_at: 'asc' as const },
+  },
+};
