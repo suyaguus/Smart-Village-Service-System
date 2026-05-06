5fa7a5ccf5f997a1e0ab0fc9b204074c02c3eb5d
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-05-06 13:19:07 +0700

    feat: membuat dto create

--- api/informasi/src/informasi/dto/create-informasi.dto.ts
+++ api/informasi/src/informasi/dto/create-informasi.dto.ts
@@ -1,1 +1,19 @@
-export class CreateInformasiDto {}
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateInformasiDto {
+  @IsString()
+  @IsNotEmpty()
+  admin_id!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  judul!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  isi!: string;
+
+  @IsString()
+  @IsOptional()
+  foto?: string;
+}
