4e0867bf0aa14e88a930eecf042b7d550f7719c0
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-05-07 12:05:16 +0700

    feat: menambahkan folder static untuk menyimpan file yang di upload

--- api/informasi/src/main.ts
+++ api/informasi/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
+import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // validationPipe
   app.useGlobalPipes(
@@ -16,6 +21,11 @@ async function bootstrap() {
   // menambahkan prefix global
   app.setGlobalPrefix('api');
 
-  await app.listen(process.env.PORT ?? 3000);
+  // menambahkan folder static untuk menyimpan file yang diupload
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads',
+  });
+
+  await app.listen(process.env.PORT ?? 3006);
 }
 void bootstrap();
