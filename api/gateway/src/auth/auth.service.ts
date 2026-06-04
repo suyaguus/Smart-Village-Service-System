7f5e4bfd4f9220c658803859107b5aeb4bc496b9
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-06-05 00:33:53 +0700

    feat: kirimkan response jika access token berhasil diperbarui

--- api/gateway/src/auth/auth.service.ts
+++ api/gateway/src/auth/auth.service.ts
@@ -54,5 +54,20 @@ export class AuthService {
   refresh(user: JwtPayload) {
     // membuat payload untuk token JWT
     const payload: JwtPayload = { username: user.username };
+
+    // kirimkan response dengan token JWT yang sudah dibuat
+    return {
+      success: true,
+      message: 'Access token berhasil diperbarui.',
+      metadata: {
+        status: HttpStatus.CREATED,
+      },
+      data: {
+        access_token: this.jwtService.sign(payload, {
+          secret: process.env.JWT_ACCESS_SECRET,
+          expiresIn: '15m',
+        }),
+      },
+    };
   }
 }
