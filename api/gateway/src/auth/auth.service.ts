188a81537aa44e7c035c298f63c6cecacd3a2d38
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-06-05 00:23:45 +0700

    feat: memvalidasi username dan password pada login

--- api/gateway/src/auth/auth.service.ts
+++ api/gateway/src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { AuthDto } from './dto/auth.dto';
 
 interface JwtPayload {
   username: string;
@@ -8,4 +9,13 @@ interface JwtPayload {
 @Injectable()
 export class AuthService {
   constructor(private jwtService: JwtService) {}
+
+  //   login
+  login(dto: AuthDto) {
+    // validasi username dan password
+    if (
+        dto.username !== process.env.ADMIN_USERNAME ||
+        dto.password !== process.env.ADMIN_PASSWORD
+    )
+  }
 }
