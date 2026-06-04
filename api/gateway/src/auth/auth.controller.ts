b68ae1791f15255b4db9ce7e82acf0d18f8bd70c
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-06-05 00:41:18 +0700

    feat: membuat controller refresh

--- api/gateway/src/auth/auth.controller.ts
+++ api/gateway/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
+import { JwtRefreshGuard } from './guards/jwt-refresh.guard';
 
 interface RequestWithUser extends Request {
   user: { username: string };
@@ -14,5 +15,12 @@ export class AuthController {
   @Post('login')
   login(@Body() dto: AuthDto) {
     return this.authService.login(dto);
+  }
+
+  //   refresh
+  @UseGuards(JwtRefreshGuard)
+  @Post('refresh')
+  refresh(@Req() req: RequestWithUser) {
+    return this.authService.refresh(req.user);
   }
 }
