64de12b142924695969df5ba083b5b40383a713a
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-06-05 14:44:38 +0700

    feat: membuat method create

--- api/gateway/src/field-surat/field-surat.service.ts
+++ api/gateway/src/field-surat/field-surat.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { field_surat_api } from 'src/common/axios/field-surat.axios';
+import { ServiceResponse } from 'src/common/interfaces/service-response.interface';
 
 @Injectable()
-export class FieldSuratService {}
+export class FieldSuratService {
+  // method create
+  async create(body: unknown): Promise<ServiceResponse> {
+    const response = await field_surat_api.post<ServiceResponse>('/', body);
+    return response.data;
+  }
+}
