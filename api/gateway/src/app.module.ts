644dab49eecd4df1c05dea4ef4dcd7871999d7cd
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-06-05 14:34:09 +0700

    feat: menambahkan import Jenis Surat Module

--- api/gateway/src/app.module.ts
+++ api/gateway/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { JenisSuratModule } from './jenis-surat/jenis-surat.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, JenisSuratModule],
   controllers: [AppController],
   providers: [AppService],
 })
