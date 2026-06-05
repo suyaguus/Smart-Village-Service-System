8a365d3811934f9d1e91bbec1d61423a768be79e
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-06-05 17:00:03 +0700

    feat: membuat method get all

--- api/gateway/src/pengajuan-surat/pengajuan-surat.controller.ts
+++ api/gateway/src/pengajuan-surat/pengajuan-surat.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { JwtAccessGuard } from 'src/auth/guards/jwt-access.guard';
 import { PengajuanSuratService } from './pengajuan-surat.service';
 
@@ -11,5 +11,11 @@ export class PengajuanSuratController {
   @Post()
   create(@Body() body: unknown) {
     return this.pengajuanSuratService.create(body);
+  }
+
+  //   method get
+  @Get()
+  findAll() {
+    return this.pengajuanSuratService.findAll();
   }
 }
