480fc048fefcc461669701a49df00f6a907c489f
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-06-05 11:36:25 +0700

    feat: membuat method findAll

--- api/gateway/src/user/user.service.ts
+++ api/gateway/src/user/user.service.ts
@@ -3,8 +3,15 @@ import { user_api } from 'src/common/axios/user.axios';
 
 @Injectable()
 export class UserService {
+  // buat fungsi create
   async create(body: unknown) {
     const response = await user_api.post('/', body);
+    return response.data;
+  }
+
+  // buat fungsi findAll
+  async findAll() {
+    const response = await user_api.get('/');
     return response.data;
   }
 }
