0fd2033c8c25087f0580895d3ac6accc1e2eda27
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-06-05 14:24:07 +0700

    feat: membuat method findAll

--- api/gateway/src/jenis-surat/jenis-surat.service.ts
+++ api/gateway/src/jenis-surat/jenis-surat.service.ts
@@ -9,4 +9,10 @@ export class JenisSuratService {
     const response = await jenis_surat_api.post<ServiceResponse>('/', body);
     return response.data;
   }
+
+  //   method findAll
+  async findAll(): Promise<ServiceResponse> {
+    const response = await jenis_surat_api.get<ServiceResponse>('/');
+    return response.data;
+  }
 }
