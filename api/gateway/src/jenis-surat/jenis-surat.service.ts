d09024b7f96c985539290bd6d35daca986952844
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-06-05 14:23:47 +0700

    feat: membuat method create

--- api/gateway/src/jenis-surat/jenis-surat.service.ts
+++ api/gateway/src/jenis-surat/jenis-surat.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { jenis_surat_api } from 'src/common/axios/jenis-surat.axios';
+import { ServiceResponse } from 'src/common/interfaces/service-response.interface';
 
 @Injectable()
-export class JenisSuratService {}
+export class JenisSuratService {
+  // method create
+  async create(body: unknown): Promise<ServiceResponse> {
+    const response = await jenis_surat_api.post<ServiceResponse>('/', body);
+    return response.data;
+  }
+}
