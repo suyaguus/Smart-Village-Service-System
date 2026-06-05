bcd1e869f9e4c74beb1cca1dc480d358bc692ac0
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-06-05 14:09:58 +0700

    feat: membuat interceptor untuk response

--- api/gateway/src/common/axios/jenis-surat.axios.ts
+++ api/gateway/src/common/axios/jenis-surat.axios.ts
@@ -1,5 +1,6 @@
 // buat variable untuk endpoint API jenis-surat
-import axios, { InternalAxiosRequestConfig } from 'axios';
+import { HttpException } from '@nestjs/common/exceptions/http.exception';
+import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
 
 export const jenis_surat_api = axios.create({
   baseURL: 'http://localhost:3002/api/jenis-surat',
@@ -14,4 +15,25 @@ jenis_surat_api.interceptors.request.use(
   },
   (error) =>
     Promise.reject(error instanceof Error ? error : new Error(String(error))),
+);
+
+// buat interceptor untuk jenis_surat_api
+jenis_surat_api.interceptors.response.use(
+  (response) => response,
+  (error: AxiosError) => {
+    // buat variable untuk response
+    const status = error.response?.status;
+
+    // pesan
+    // const message = error.response?.data?.message;
+    const message = error.response?.data;
+
+    // jika status error (terdfinisi)
+    if (status && message) {
+      throw new HttpException(message, status);
+    }
+
+    // jika status tidak terdefinisi
+    throw new HttpException('Jenis Surat Service Error', 500);
+  },
 );
