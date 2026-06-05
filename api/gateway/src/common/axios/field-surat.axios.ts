e3078a5cd2fbba44579ab6b320e23fb2df3c0b43
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-06-05 17:09:38 +0700

    document: menambahkan comment

--- api/gateway/src/common/axios/field-surat.axios.ts
+++ api/gateway/src/common/axios/field-surat.axios.ts
@@ -20,13 +20,19 @@ field_surat_api.interceptors.request.use(
 field_surat_api.interceptors.response.use(
   (response) => response,
   (error: AxiosError) => {
+    // buat variable untuk response
     const status = error.response?.status;
+
+    // pesan
+    // const message = error.response?.data?.message;
     const message = error.response?.data;
 
+    // jika status error (terdfinisi)
     if (status && message) {
       throw new HttpException(message, status);
     }
 
+    // jika status tidak terdefinisi
     throw new HttpException('Field Surat Service Error', 500);
   },
 );
