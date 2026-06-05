4ae240f3319e8156e3d2e6bf8e5498c05fe9f4d5
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-06-05 14:42:30 +0700

    feat: membuat interceptor untuk response

--- api/gateway/src/common/axios/field-surat.axios.ts
+++ api/gateway/src/common/axios/field-surat.axios.ts
@@ -1,4 +1,5 @@
-import axios, { InternalAxiosRequestConfig } from 'axios';
+import { HttpException } from '@nestjs/common';
+import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
 
 export const field_surat_api = axios.create({
   baseURL: 'http://localhost:3003/api/field-surat',
@@ -13,4 +14,19 @@ field_surat_api.interceptors.request.use(
   },
   (error) =>
     Promise.reject(error instanceof Error ? error : new Error(String(error))),
+);
+
+// buat interceptor untuk field_surat_api
+field_surat_api.interceptors.response.use(
+  (response) => response,
+  (error: AxiosError) => {
+    const status = error.response?.status;
+    const message = error.response?.data;
+
+    if (status && message) {
+      throw new HttpException(message, status);
+    }
+
+    throw new HttpException('Field Surat Service Error', 500);
+  },
 );
