a607f907970700ff5872149bbb2615e718614067
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-06-05 11:44:07 +0700

    feat: membuat method get by id

--- api/gateway/src/user/user.controller.ts
+++ api/gateway/src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Post, Body, UseGuards, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { JwtAccessGuard } from 'src/auth/guards/jwt-access.guard';
 import { UserService } from './user.service';
 
@@ -17,5 +27,11 @@ export class UserController {
   @Get()
   findAll() {
     return this.userService.findAll();
+  }
+
+  //   method get by id
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.findOne(id);
   }
 }
