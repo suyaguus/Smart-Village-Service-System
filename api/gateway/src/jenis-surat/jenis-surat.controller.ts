b9c5de440aec132f29ddfaa7edc337c6317ffd67
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-06-05 14:31:34 +0700

    feat: membuat method patch

--- api/gateway/src/jenis-surat/jenis-surat.controller.ts
+++ api/gateway/src/jenis-surat/jenis-surat.controller.ts
@@ -4,6 +4,7 @@ import {
   Get,
   Param,
   ParseIntPipe,
+  Patch,
   Post,
   UseGuards,
 } from '@nestjs/common';
@@ -31,5 +32,11 @@ export class JenisSuratController {
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.jenisSuratService.findOne(id);
+  }
+
+  //   method patch
+  @Patch(':id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() body: unknown) {
+    return this.jenisSuratService.update(id, body);
   }
 }
