6987f4464f69fdaba3a61c95db4491cd559b8896
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-06-05 14:49:59 +0700

    feat: membuat method get by jenis-surat

--- api/gateway/src/field-surat/field-surat.controller.ts
+++ api/gateway/src/field-surat/field-surat.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtAccessGuard } from 'src/auth/guards/jwt-access.guard';
 import { FieldSuratService } from './field-surat.service';
 
@@ -17,5 +25,13 @@ export class FieldSuratController {
   @Get()
   findAll() {
     return this.fieldSuratService.findAll();
+  }
+
+  //   method get by jenis-surat
+  @Get('jenis-surat/:jenis_surat_id')
+  findByJenisSurat(
+    @Param('jenis_surat_id', ParseIntPipe) jenis_surat_id: number,
+  ) {
+    return this.fieldSuratService.findByJenisSurat(jenis_surat_id);
   }
 }
