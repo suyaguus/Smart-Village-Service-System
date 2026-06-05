94b2b3dd440556321cc419374e68065da8f23953
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-06-05 11:31:35 +0700

    feat: membuat interceptor untuk request

--- api/gateway/src/common/axios/user.axios.ts
+++ api/gateway/src/common/axios/user.axios.ts
@@ -1,8 +1,18 @@
 // buat variable untuk endpoint API kategori
 
-import axios from 'axios';
+import axios, { InternalAxiosRequestConfig } from 'axios';
 
 export const user_api = axios.create({
   baseURL: 'http://localhost:3001/api/user',
   timeout: 5000,
 });
+
+// buat interceptor untuk request
+user_api.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    config.headers['x-internal-secret'] = process.env.INTERNAL_SECRET;
+    return config;
+  },
+  (error) =>
+    Promise.reject(error instanceof Error ? error : new Error(String(error))),
+);
