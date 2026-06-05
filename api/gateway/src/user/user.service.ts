a44d3578c00d5029e877dc681bf2a00b2030f584
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-06-05 11:50:19 +0700

    feat: membuat interface untuk user service

--- api/gateway/src/user/user.service.ts
+++ api/gateway/src/user/user.service.ts
@@ -1,35 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { user_api } from 'src/common/axios/user.axios';
 
+// interface response dari user-service
+interface ServiceResponse {
+  success: boolean;
+  message: string;
+  metadata: { status: number; total_data?: number };
+  data?: unknown;
+}
+
 @Injectable()
 export class UserService {
   // buat fungsi create
-  async create(body: unknown) {
-    const response = await user_api.post('/', body);
+  async create(body: unknown): Promise<ServiceResponse> {
+    const response = await user_api.post<ServiceResponse>('/', body);
     return response.data;
   }
 
   // buat fungsi findAll
-  async findAll() {
-    const response = await user_api.get('/');
+  async findAll(): Promise<ServiceResponse> {
+    const response = await user_api.get<ServiceResponse>('/');
     return response.data;
   }
 
   // buat fungsi findOne
-  async findOne(id: number) {
-    const response = await user_api.get(`/${id}`);
+  async findOne(id: number): Promise<ServiceResponse> {
+    const response = await user_api.get<ServiceResponse>(`/${id}`);
     return response.data;
   }
 
-  //   buat fungsi update
-  async update(id: number, body: unknown) {
-    const response = await user_api.patch(`/${id}`, body);
+  // buat fungsi update
+  async update(id: number, body: unknown): Promise<ServiceResponse> {
+    const response = await user_api.patch<ServiceResponse>(`/${id}`, body);
     return response.data;
   }
 
-  //   buat fungsi remove
-  async remove(id: number) {
-    const response = await user_api.delete(`/${id}`);
+  // buat fungsi remove
+  async remove(id: number): Promise<ServiceResponse> {
+    const response = await user_api.delete<ServiceResponse>(`/${id}`);
     return response.data;
   }
 }
