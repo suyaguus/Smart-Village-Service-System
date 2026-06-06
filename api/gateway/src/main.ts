7f2f5327a0a3f87461b99fd746f0f7eb4a1bb317
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-06-06 14:14:25 +0700

    feat: menambahkan cors dan global filters

--- api/gateway/src/main.ts
+++ api/gateway/src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // mengaktifkan CORS agar CMS dan user app bisa mengakses gateway
+  app.enableCors();
 
   // validationPipe
   app.useGlobalPipes(
@@ -15,6 +19,10 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  // global exception filter untuk standarisasi error response
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   // menambahkan prefix global
   app.setGlobalPrefix('api');
