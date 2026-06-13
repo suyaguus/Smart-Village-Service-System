908c303783d672987526562cb9b474d0767d632c
Fito016 <alfitof919@gmail.com>
2026-06-13 20:09:46 +0700

    feat: create informasi desa page

---
+++ cms/src/app/dashboard/informasi/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React, { useState } from 'react';
+import { Plus, Edit2, Trash2, Eye, Clock, ArrowLeft, Save, Send, Image as ImageIcon } from 'lucide-react';
+import { Card } from '@/components/ui/card';
+
+
+
+
+export default function InformasiDesaPage() {
+
+}
