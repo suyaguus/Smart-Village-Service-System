f3f0439dc4838ed546c3d9402906e7a97df0adbe
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-06-13 21:34:43 +0700

    feat: meanambahkan roles guard ke controller

--- api/gateway/src/field-surat/field-surat.controller.ts
+++ api/gateway/src/field-surat/field-surat.controller.ts
@@ -13,9 +13,11 @@ import { JwtAccessGuard } from 'src/auth/guards/jwt-access.guard';
 import { FieldSuratService } from './field-surat.service';
 import { CreateFieldSuratDto } from './dto/create-field-surat.dto';
 import { UpdateFieldSuratDto } from './dto/update-field-surat.dto';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
 @Controller('field-surat')
-@UseGuards(JwtAccessGuard)
+@UseGuards(JwtAccessGuard, RolesGuard)
 export class FieldSuratController {
   constructor(private readonly fieldSuratService: FieldSuratService) {}
 
@@ -26,12 +28,14 @@ export class FieldSuratController {
   }
 
   //   method get
+  @Roles('USER')
   @Get()
   findAll() {
     return this.fieldSuratService.findAll();
   }
 
   //   method get by jenis-surat
+  @Roles('USER')
   @Get('jenis-surat/:jenis_surat_id')
   findByJenisSurat(
     @Param('jenis_surat_id', ParseIntPipe) jenis_surat_id: number,
@@ -40,6 +44,7 @@ export class FieldSuratController {
   }
 
   //   method get by id
+  @Roles('USER')
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.fieldSuratService.findOne(id);
