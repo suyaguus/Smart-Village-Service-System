fcc1ad9c373a6878b5e8fafde3515953212de649
Fito016 <alfitof919@gmail.com>
2026-06-14 23:06:31 +0700

    feat: add state untuk menyimpan filter yang sedang aktif

--- cms/src/app/dashboard/pengguna/page.tsx
+++ cms/src/app/dashboard/pengguna/page.tsx
@@ -5,5 +5,7 @@
 
 
 export default function ManajemenPenggunaPage() {
-    
+
+  // 1. State untuk menyimpan filter yang sedang aktif ('ALL', 'ADMIN', atau 'USER')
+  const [activeFilter, setActiveFilter] = useState('ALL');
 }
